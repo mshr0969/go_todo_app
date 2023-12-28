79acd8d75e9ee15f0c8a907e1ed4c1655ef1205b
mshr0969 <mshr60531@gmail.com>
2023-12-28 16:17:58 +0900

    simple server

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github/mshr0969/go_todo_app
+
+go 1.21.5
+
+require golang.org/x/sync v0.5.0
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func main(){
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminate server: %v", err)
+	}
+}
+
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		}),
+	}
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		if err := s.ListenAndServe(); err != nil &&
+		err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdhown: %+v", err)
+	}
+
+	return eg.Wait()
+}
