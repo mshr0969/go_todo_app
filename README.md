497e1c8e657b9bd726c2a8026abdf47532961920
mshr0969 <mshr60531@gmail.com>
2024-01-03 22:44:56 +0900

    separate server and use mux

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## go_todo_app
 
-ホットリロード：https://github.com/cosmtrek/air
+## 参考
+https://github.com/cosmtrek/air
+https://github.com/caarlos0/env
--- main.go
+++ main.go
@@ -6,10 +6,7 @@ import (
 	"github/mshr0969/go_todo_app/config"
 	"log"
 	"net"
-	"net/http"
 	"os"
-
-	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -30,24 +27,7 @@ func run(ctx context.Context) error {
 	}
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
-	s := &http.Server{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	}
-	eg, ctx := errgroup.WithContext(ctx)
-	eg.Go(func() error {
-		if err := s.Serve(l); err != nil &&
-			err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdhown: %+v", err)
-	}
-
-	return eg.Wait()
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
