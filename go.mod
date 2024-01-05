4553c5b4125448e0555e244741e2290d71c65f82
mshr0969 <mshr60531@gmail.com>
2024-01-06 01:06:16 +0900

    add entity, store, helper

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.21.5
 
 require (
 	github.com/caarlos0/env/v6 v6.10.1
+	github.com/google/go-cmp v0.6.0
 	golang.org/x/sync v0.5.0
 )
---
+++ store/store.go
@@ -0,0 +1,27 @@
+package store
+
+import "github/mshr0969/go_todo_app/entity"
+
+var (
+	Tasks = &TaskStore{Tasks: map[entity.TaskID]*entity.Task{}}
+)
+
+type TaskStore struct {
+	LastID entity.TaskID
+	Tasks  map[entity.TaskID]*entity.Task
+}
+
+func (ts *TaskStore) Add(t *entity.Task) (entity.TaskID, error) {
+	ts.LastID++
+	t.ID = ts.LastID
+	ts.Tasks[t.ID] = t
+	return t.ID, nil
+}
+
+func (ts *TaskStore) All() entity.Tasks {
+	tasks := make([]*entity.Task, len(ts.Tasks))
+	for i, t := range ts.Tasks {
+		tasks[i-1] = t
+	}
+	return tasks
+}
